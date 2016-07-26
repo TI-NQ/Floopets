ead78df53ed66d5411ec6f87c00aa93c34c7bf11
villadac <ccvillada4@misena.edu.co>
2016-07-26 11:41:21 -0500

    completando los class

--- Model/adopcion.class.php
+++ Model/adopcion.class.php
@@ -1,7 +1,7 @@
 <?php
 	class gestion_adopcion
 	{
-		// Metodo Create()		
+		// Metodo Create()
 		function Create($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora)
 		{
 			//Instanciamos y nos conectamos a la bd
@@ -12,6 +12,45 @@
 			$query = $conexion->prepare($consulta);
 			$query->execute(array($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora));
 			floopets_BD::Disconect();
+		}
+		function ReadAll()
+		{
+				//Instanciamos y nos conectamos a la bd
+				$Conexion = floopets::Connect();
+				$Conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				//Crear el query que vamos a realizar
+				$consulta = "SELECT * FROM adopcion";
+				$query = $Conexion->prepare($consulta);
+				$query->execute();
+				//Devolvemos el resultado en un arreglo
+				//Fetch: es el resultado que arroja la consulta en forma de un vector o matriz segun sea el caso
+				//Para consultar donde arroja mas de un dato el fatch debe ir acompañado con la palabra ALL
+				$resultado = $query->fetchALL(PDO::FETCH_BOTH);
+				return $resultado;
+				floopets::Disconnect();
+		}
+
+		function Update($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora)
+		{
+			//Instanciamos y nos conectamos a la bd
+			$Conexion = floopets::Connect();
+			$Conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			//Crear el query que vamos a realizar
+			$consulta = "UPDATE adopcion SET to_nombre=?,ani_cod_animal=?,usu_cod_usuario=?,ado_fecha=?,ado_hora=? WHERE ado_cod_adopcion = ?" ;
+			$query = $Conexion->prepare($consulta);
+			$query->execute(array($ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora,$ado_cod_adopcion));
+			floopets::Disconnect();
+		}
+			function Delete($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora)
+			{
+				//Instanciamos y nos conectamos a la bd
+				$Conexion = floopets::Connect();
+				$Conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				//Crear el query que vamos a realizar
+				$consulta = "DELETE FROM adopcion WHERE ado_cod_adopcion = ?" ;
+				$query = $Conexion->prepare($consulta);
+				$query->execute(array($ado_cod_adopcion));
+				floopets::Disconnect();
 		}
 
 	}
