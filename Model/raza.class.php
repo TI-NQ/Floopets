b1ff83abc0f8a69092d95a9b1189bec5608f904e
villadac <ccvillada4@misena.edu.co>
2016-07-26 11:00:24 -0500

    ..

---
+++ Model/raza.class.php
@@ -0,0 +1,19 @@
+<?php
+	class gestion_raza
+	{
+		// Metodo Create()
+		// El metodo guarda los datos en la tabla contactos, captura todos los parametros desde el formulario.
+		function Create($ra_cod_raza,$ra_nombre,$ta_cod_tipo_animal)
+		{
+			//Instanciamos y nos conectamos a la bd
+			$conexion=floopets_BD::Connect();
+			$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			//Crear el query que vamos a realizar.
+			$consulta ="INSERT INTO tipo_animal (ra_cod_raza,ra_nombre,ta_cod_tipo_animal) VALUES (?,?,?)";
+			$query = $conexion->prepare($consulta);
+			$query->execute(array($ra_cod_raza,$ra_nombre,$ta_cod_tipo_animal));
+			floopets_BD::Disconect();
+		}
+
+	}
+?>
