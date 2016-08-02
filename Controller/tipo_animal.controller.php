68ba2440e0af5ae2ac95e47eb19b8ba849bd9c24
Andrea0906 <saorozco17@misena.edu.co>
2016-08-02 07:38:37 -0500

    .-.

---
+++ Controller/tipo_animal.controller.php
@@ -0,0 +1,48 @@
+<?php 
+	require_once("../Model/conexion.php");
+	require_once("../Model/tipo_animal.class.php");
+
+	$accion = $_REQUEST["accion"];
+	switch ($accion) {
+		case 'c':
+				
+			$	= $_POST[""];
+			$	= $_POST[""];
+
+			try {
+				Gestion_tipo_animal::Create($);
+				$mensaje = "Se creo exitosamente";
+			} catch (Exception $e) {
+				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
+			}
+			header("Location: ../View/gestion_cuidado.php?m=$mensaje");
+
+			break;
+			case 'u':
+				$cu_cod_cuidado		= $_POST["cu_cod_cuidado"];
+				$cu_nombre			= $_POST["cu_nombre"];
+				$cu_descripcion 	= $_POST["cu_descripcion"];
+
+			try {
+				Gestion_cuidado::Update($cu_cod_cuidado,$cu_nombre,$cu_descripcion);
+				$mensaje = "Se actializo exitosamente";
+			} catch (Exception $e) {
+				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
+			}
+			header("Location: ../View/gestion_cuidado.php?m=$mensaje");
+			break;
+		
+		case 'd':
+			try {
+          $cuidado = Gestion_cuidado::Delete(base64_decode($_REQUEST["cu"]));
+          $mensaje = "se elimino correctamente";
+          header("Location: ../View/gestion_cuidado.php?m=".$mensaje);
+        } catch (Exception $e) {
+          $msn = "error:".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
+          header("Location: ../View/gestion_cuidado.php?m=".$mensaje);
+        }
+      
+			break;
+	}
+
+ ?>	
