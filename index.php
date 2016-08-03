47d5f729ebac0f1dc4ea475ab84a578ea09abf69
Richardochoa <rlochoa1@misena.edu.co>
2016-08-03 00:19:37 -0500

    maqueta web

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<!--Import Google Icon Font-->
+	    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	    <!--Import materialize.css-->
+	    <link type="text/css" rel="stylesheet" href="WebFloopets/materialize/css/materialize.css"  media="screen,projection"/>
+		<link rel="shortcut icon" type="image/x-icon" href="WebFloopets/images/title-web.ico">
+		<meta charset="UTF-8">
+		<title>Floopets-Cambiando vidas</title>
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale?1.0, minimum-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="WebFloopets/css/estilos.css">
+	</head>
+	<body>
+		<div class="banner-video">
+			<video src="WebFloopets/video/563398388.mp4" autoplay loop>
+			</video>
+		</div>
+		<div class="row">
+				<div class="container__perros col s12 m6 l6">
+					<img src="WebFloopets/images/1.jpg">
+				</div>
+				<div class="container__gatos col s12 m6 l6">
+					<img src="WebFloopets/images/5.jpg">
+				</div>
+				<img class="nub" src="WebFloopets/recursos/images/linea-azul.png">
+		</div>
+		<div class="row">
+			<div class="container__img col s12 m4 l4" >
+				<img src="WebFloopets/recursos/images/nube-1.png">
+			</div>
+			<div class="container__form__help col s12 m8 l8">
+			    <!--<form class="col s12">
+			      <div class="row">
+			        <div class="input-field col s6">
+			          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+			          <label for="first_name">First Name</label>
+			        </div>
+			        <div class="input-field col s6">
+			          <input id="last_name" type="text" class="validate">
+			          <label for="last_name">Last Name</label>
+			        </div>
+			      </div>
+			      <div class="row">
+			        <div class="input-field col s12">
+			          <input disabled value="I am not editable" id="disabled" type="text" class="validate">
+			          <label for="disabled">Disabled</label>
+			        </div>
+			      </div>
+			      <div class="row">
+			        <div class="input-field col s12">
+			          <input id="password" type="password" class="validate">
+			          <label for="password">Password</label>
+			        </div>
+			      </div>
+			      <div class="row">
+			        <div class="input-field col s12">
+			          <input id="email" type="email" class="validate">
+			          <label for="email">Email</label>
+			        </div>
+			      </div>
+			    </form>-->
+			</div>
+			<img class="mon" src="WebFloopets/recursos/images/montañas.png">
+		</div>
+	</body>
+		<!--Import jQuery before materialize.js-->
+      	<script type="text/javascript" src="WebFloopets/js/jquery-3.1.0.js">
+      	</script>
+      	<script type="text/javascript" src="WebFloopets/materialize/js/materialize.js"></script>
+</html>
