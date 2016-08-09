a128ed32767b87fee1c5520c20824db7c1c1dbdb
Richardochoa <rlochoa1@misena.edu.co>
2016-08-09 10:57:16 -0500

    ...

--- index.php
+++ index.php
@@ -68,6 +68,10 @@
     						<label>Selecciona un tipo de denuncia</label>
   						</div>
 				      </div>
+        			  <div class="input-field col s12 textarea">
+          			      <textarea id="textarea1" class="materialize-textarea"></textarea>
+          				  <label for="textarea1">Descripcion</label>
+        			  </div>
 				        <div class="input-field col s6">
 				          <input id="last_name" type="text" class="validate">
 				          <label for="last_name">Last Name</label>
