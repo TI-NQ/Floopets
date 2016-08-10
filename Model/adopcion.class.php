5d547cc12589d48c3d7b5181d04ee5657d03f42d
Andrea0906 <saorozco17@misena.edu.co>
2016-08-10 07:50:46 -0500

    :walking:

--- Model/adopcion.class.php
+++ Model/adopcion.class.php
@@ -1,22 +1,22 @@
 <?php
-	class gestion_adopcion
+	class Gestion_adopcion
 	{
 		// Metodo Create()
-		function Create($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora)
+		function Create($ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora,$ado_imagen)
 		{
 			//Instanciamos y nos conectamos a la bd
 			$conexion=floopets_BD::Connect();
 			$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			//Crear el query que vamos a realizar.
-			$consulta ="INSERT INTO adopcion (ado_cod_adopcion,ani_cod_animal,usu_cod_usuario,ado_fecha,ado_hora) VALUES (?,?,?,?,?)";
+			$consulta ="INSERT INTO adopcion (ani_cod_animal,usu_cod_usuario,ado_fecha,ado_hora,ado_imagen) VALUES (?,?,?,?,?)";
 			$query = $conexion->prepare($consulta);
-			$query->execute(array($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora));
+			$query->execute(array($ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora,$ado_imagen));
 			floopets_BD::Disconnect();
 		}
 		function ReadAll()
 		{
 				//Instanciamos y nos conectamos a la bd
-				$Conexion = floopets::Connect();
+				$Conexion = floopets_BD::Connect();
 				$Conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				//Crear el query que vamos a realizar
 				$consulta = "SELECT * FROM adopcion";
@@ -46,21 +46,21 @@
 			floopets_BD::Disconnect();
 		}
 
-		function Update($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora)
+		function Update($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora,$ado_imagen)
 		{
 			//Instanciamos y nos conectamos a la bd
-			$Conexion = floopets::Connect();
+			$Conexion = floopets_BD::Connect();
 			$Conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			//Crear el query que vamos a realizar
-			$consulta = "UPDATE adopcion SET to_nombre=?,ani_cod_animal=?,usu_cod_usuario=?,ado_fecha=?,ado_hora=? WHERE ado_cod_adopcion = ?" ;
+			$consulta = "UPDATE adopcion SET ani_cod_animal=?,usu_cod_usuario=?,ado_fecha=?,ado_hora=? WHERE ado_cod_adopcion = ?" ;
 			$query = $Conexion->prepare($consulta);
 			$query->execute(array($ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora,$ado_cod_adopcion));
 			floopets_BD::Disconnect();
 		}
-			function Delete($ado_cod_adopcion,$ani_cod_animal,$usu_cod_usuario,$ado_fecha,$ado_hora)
+			function Delete($ado_cod_adopcion)
 			{
 				//Instanciamos y nos conectamos a la bd
-				$Conexion = floopets::Connect();
+				$Conexion = floopets_BD::Connect();
 				$Conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				//Crear el query que vamos a realizar
 				$consulta = "DELETE FROM adopcion WHERE ado_cod_adopcion = ?" ;
---
+++ View/actualizar_adopcion.php
@@ -0,0 +1,32 @@
+<?php 
+require_once("../Model/conexion.php");
+ require_once("../Model/adopcion.class.php");
+ $adopcion =  Gestion_adopcion::ReadbyID(base64_decode($_REQUEST["ad"]));
+
+// echo $cu[0];
+ ?>
+<form action="../Controller/adopcion.controller.php" method="POST">
+<input type="hidden" readonly name="ado_cod_adopcion" required value="<?php echo $adopcion[0] ?>">
+<input type="hidden" readonly name="ani_cod_animal" required value="<?php echo $adopcion[4] ?>">
+<input type="hidden" readonly name="usu_cod_usuario" required value="<?php echo $adopcion[5] ?>">
+	<div class="form-group">
+		<label class="form-label">fecha</label>
+		<input class="form-control" type="date" name="ado_fecha" required  value="<?php echo $adopcion[1] ?>">
+	</div>
+	<div class="form-group">
+		<label class="form-label">hora</label>
+		<input class="form-control" type="time" name="ad_descripcion" required  value="<?php echo $adopcion[2] ?>">
+	</div>
+<div class="file-field input-field col s12 m6">
+                <div class="btn">
+                  <span>imagen</span>
+                  <input type="file" multiple name="Imagen_Upload[]">
+                </div>
+                <div class="file-path-wrapper">
+                  <input class="file-path validate"  type="text" placeholder="" name="ado_imagen" value="<?php echo $adopcion[3] ?>" >
+                </div>
+              </div>
+	<div class="form-group">
+		<button name="accion" value="c" class="btn btn-primary">Registrar</button>
+	</div>
+</form>
