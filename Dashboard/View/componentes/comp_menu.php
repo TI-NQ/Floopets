77ce45ecf9540fafe04516414e4db6bbefd6db72
Richardochoa <rlochoa1@misena.edu.co>
2016-09-06 10:58:27 -0500

    ...

--- Dashboard/View/componentes/comp_menu.php
+++ Dashboard/View/componentes/comp_menu.php
@@ -1,4 +1,31 @@
-<li><a href=""><i class="icono izquierda fa fa-home"></i> Inicio</a></li>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- <li><a href=""><i class="icono izquierda fa fa-home"></i> Inicio</a></li>
 <li><a href="dashboard.php?p=<?php echo base64_encode('registrar_organizacion')?>"><i class="icono izquierda fa fa-star"></i> Gestion Adopcion<i class="icono derecha fa fa-chevron-down"></i> </a>
 </li>
-<li><a href=""><i class="icono izquierda fa fa-star"></i> Gestionar Donacion </a></li>
+<li><a href=""><i class="icono izquierda fa fa-star"></i> Gestionar Donacion </a></li> -->
+<div class="dropdown">
+  <!-- <li role="presentation" > -->
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+      <li><a href="#">HTML</a></li>
+      <li><a href="#">CSS</a></li>
+      <li><a href="#">JavaScript</a></li>
+    </ul>
+  </div>
+
+    <!-- <a id="menu" class="dropdown-toggle" data-toggle="dropdown" href="#">
+      Dropdown <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+    </ul>
+  </li>
+  <ul class="dropdown" aria-labelledby="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul> -->
