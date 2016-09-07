e2bab330f2b0eb308d2bd6025dba41816b598ea6
Richardochoa <rlochoa1@misena.edu.co>
2016-09-06 23:06:05 -0500

    ...

--- Dashboard/View/componentes/comp_menu.php
+++ Dashboard/View/componentes/comp_menu.php
@@ -5,42 +5,42 @@
 <ul class="collapsible" data-collapsible="accordion">
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-building-o" aria-hidden="true"></i>Organizacion</a></div>
-      <div class="collapsible-body"><p><a href="dashboard.php?p=<?php echo base64_encode('registrar_organizacion')?>">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="dashboard.php?p=<?php echo base64_encode('registrar_organizacion')?>">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-paw" aria-hidden="true"></i>Animales</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-paw" aria-hidden="true"></i>Adopciones</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-calendar" aria-hidden="true"></i>Eventos</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-money" aria-hidden="true"></i>Donaciones</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-shield" aria-hidden="true"></i>Denuncias</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-eyedropper" aria-hidden="true"></i>Vacunas</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-plus" aria-hidden="true"></i>Voluntarios</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-users" aria-hidden="true"></i>Usuarios</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
     <li>
       <div class="collapsible-header grey lighten-2"><a href="#"><i class="fa fa-paw" aria-hidden="true"></i>Cuidados</a></div>
-      <div class="collapsible-body"><p><a href="#">Nuevo</a></p></div>
+      <div class="collapsible-body"><p><a class="item_menu" href="#">Nuevo</a></p></div>
     </li>
   </ul>
