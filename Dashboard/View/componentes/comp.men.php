1daf40bde4b15dcc89c65d77379f4183110a5c7a
Richardochoa <rlochoa1@misena.edu.co>
2016-09-21 09:59:17 -0500

    ......

---
+++ Dashboard/View/componentes/comp.men.php
@@ -0,0 +1,30 @@
+<li class="divider" style="margin:30px;"></li>
+<ul class="collapsible" data-collapsible="accordion">
+  <style media="screen">
+  .collapsible-header {
+      border-bottom: 1px solid #bbb;}
+  </style>
+<?php
+// 1=usuario
+if($_SESSION["cod_rol"]==1){
+?>
+<li>
+  <div class="collapsible-header grey lighten-2"><a><i class="fa fa-paw" aria-hidden="true"></i>Animales</a></div>
+  <div class="collapsible-body"><p><a class="item_menu" href="dashboard.php?p=<?php echo base64_encode('registrar_animal')?>">Crear</a></p></div>
+  <div class="collapsible-body"><p><a class="item_menu" href="dashboard.php?p=<?php echo base64_encode('gestion_animal')?>">Gestionar</a></p></div>
+</li>
+  <!-- aqui metan el menu -->
+<?php
+// 2=administrador
+}elseif($_SESSION["cod_rol"]==2){
+?>
+<!-- aqui metan el menu -->
+<?php
+// 3=organizacion
+  }elseif ($_SESSION["cod_rol"]==3) {
+
+?>
+      <!-- aqui metan el menu -->
+<?php
+}
+?>
