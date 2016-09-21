1b937a92b04cc496424444a41d86967e713b5c46
Andreagzmn <avguzman61@misena.edu.co>
2016-09-21 07:25:01 -0500

    ...

--- Dashboard/View/Gestion_denuncia.php
+++ Dashboard/View/Gestion_denuncia.php
@@ -53,7 +53,7 @@ $("a#btntrash").click(function(){
                         <li>".$row["de_nombre"]."</li>
                       </div>
                       <div class='imagenmascota col l7'>
-                          <img class='img-circle' style='width:180px ;height:180px ;' src='img/imagen_denuncia/".$row["de_contacto"]."/".$row["de_imagen"]."'>
+                          <img class='circle responsive-img' style='width:180px ;height:180px ;' src='img/imagen_denuncia/".$row["de_contacto"]."/".$row["de_imagen"]."'>
                       </div>
                       <div class='col l6'>
                           <a href='../View/actualizar_denuncia.php?dn=".base64_encode($row["de_cod_denuncia"])."'>
