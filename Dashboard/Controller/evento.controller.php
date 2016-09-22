6a6c531294e56f9578c2cc5b735437b78f610aeb
Andrea0906 <saorozco17@misena.edu.co>
2016-09-22 11:29:44 -0500

    .

--- Dashboard/Controller/evento.controller.php
+++ Dashboard/Controller/evento.controller.php
@@ -28,6 +28,7 @@
 			} catch (Exception $e) {
 				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
 			}
+			 // header("Location: ../View/dashboard.php?p=".base64_encode("gestion_evento"));
 
 			break;
 			case 'u':
