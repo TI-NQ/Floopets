6d699fe7be364cd02a109c91e75b4e775edb92a8
Andrea0906 <saorozco17@misena.edu.co>
2016-09-26 18:45:32 -0500

    .

--- Dashboard/Controller/evento.controller.php
+++ Dashboard/Controller/evento.controller.php
@@ -28,12 +28,16 @@
 			} catch (Exception $e) {
 				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
 			}
-			 // header("Location: ../View/dashboard.php?p=".base64_encode("gestion_evento"));
+			header("location: ../View/gestion_evento.php");
+			 //header("Location: ../View/dashboard.php?p=".base64_encode("gestion_evento"));
 
 			break;
 			case 'u':
-      	$te_cod_tipo_evento      =$_POST["te_cod_tipo_evento"];
+			$eve_cod_evento      =$_POST["eve_cod_evento"];
+      $te_cod_tipo_evento      =$_POST["te_cod_tipo_evento"];
 		$eve_nombre              =$_POST["eve_nombre"];
+		$nombre_eve_imagen 	= strtolower(str_replace('ñ', 'n', $eve_nombre));
+		$nombre_eve_imagen 	= strtolower(str_replace(' ', '', $nombre_eve_imagen));
       	$eve_direccion            =$_POST["eve_direccion"];
       	$eve_fecha           =$_POST["eve_fecha"];
       	$eve_fecha_hasta           =$_POST["eve_fecha_hasta"];
@@ -42,14 +46,19 @@
       	$eve_descripcion           =$_POST["eve_descripcion"];
       	$eve_imagen 				=$_POST["eve_imagen"];
       	$eve_estado 				=$_POST["eve_estado"];
+     	$count_galeria			= count($_FILES['eve_imagen']['name']);
 
 			try {
+				if($count_galeria >= 1){
+					include("Upload_eve_imagen.php");
+				} 
 				Gestion_evento::Update($eve_cod_evento,$te_cod_tipo_evento,$eve_nombre,$eve_direccion,$eve_fecha,$eve_fecha_hasta,$eve_hora,$eve_hora_hasta,$eve_descripcion,$eve_imagen,$eve_estado);
 				$mensaje = "Se actializo exitosamente";
 			} catch (Exception $e) {
 				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
 			}
-			header("Location: ../View/dashboard.php?p=".base64_encode("gestion_evento"));
+			header("location: ../View/gestion_evento.php");
+			//header("Location: ../View/dashboard.php?p=".base64_encode("gestion_evento"));
 			break;
 
 		case 'd':
--- Dashboard/View/actualizar_evento.php
+++ Dashboard/View/actualizar_evento.php
@@ -2,34 +2,74 @@
 	require_once("../Model/conexion.php");
     require_once("../Model/evento.class.php");
 	$evento=Gestion_evento::ReadbyID(base64_decode($_REQUEST["eve"]));
+	require_once("../Model/tipo_evento.class.php");
+	$eve = Gestion_tipo_evento::ReadAll();
  ?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<form action="../Controller/evento.controller.php" method="POST">
-	<h1>Actualizar Evento</h1>
-	<div class="form-group">
-		<label>Codigo evento:</label>
-		<input name="eve_cod_evento"value="<?php echo $evento[0]?>"readonly></input>
-		<br>
-		<label>Codigo tipo evento:</label>
-		<input name="te_cod_tipo_evento"value="<?php echo $evento[1]?>"></input>
-		<br>
-		<label>Nombre:</label>
-		<input name="eve_nombre" value="<?php echo $evento[2]?>"></input>
-		<br>
-		<label>Dirección:</label>
-		<input name="eve_direccion"value="<?php echo $evento[3]?>"></input>
-		<br>
-		<label>Fecha:</label>
-		<input type="date" name="eve_fecha"value="<?php echo $evento[4]?>"></input>
-		<br>
-		<label>Hora:</label>
-		<input type="time" name="eve_hora"value="<?php echo $evento[5]?>"></input>
-		<br>
-		<label>Descripcion:</label>
-		<input name="eve_descripcion" value="<?php echo $evento[5]?>"></input>
-		<br>
+<form action="../Controller/evento.controller.php" method="POST" enctype="multipart/form-data">
+	<h1>Registrar Evento</h1>
+	<input class="file-path validate"  type="hidden" name="eve_cod_evento" value="<?php echo $evento[0] ?>" >
+	<div class="form-group">	
 
+		<label>Tipo evento:</label>
+		<select name="te_cod_tipo_evento"  required>
+				<?php
+					foreach ($eve as $row) {
+						echo "<option value = '".$row["te_cod_tipo_evento"]."'>".$row["te_nombre"]."</option>";
+					}
+				 ?>
+		</select>
+		</div>
+		<br>
+		<div class="form-group">
+		<label class="form-label">nombre</label>
+		<input class="form-control" type="text" name="eve_nombre" value="<?php echo $evento[2] ?>"  required>
 	</div>
+		<br>
+		<div class="form-group">
+		<label class="form-label">direccion</label>
+		<input class="form-control" type="text" name="eve_direccion" value="<?php echo $evento[3] ?>" required>
+	</div>
+		<br>
+		<div class="col s12 m12">
+              <p class="center">fecha</p>
+               <label>desde</label>
+                  <input type="date" id="demo" name="eve_fecha" value="<?php echo $evento[4] ?>"  required>
+                  <label>Hasta</label>
+                  <input type="date" id="demo" name="eve_fecha_hasta" value="<?php echo $evento[5] ?>"  required>                 
+            </div>
+		<br>
+		<div class="col s12 m12">
+              <p class="center">Horario</p>
+               <label>desde</label>
+                  <input type="time" id="demo" name="eve_hora" value="<?php echo $evento[6] ?>"  required>
+                  <label>Hasta</label>
+                  <input type="time" id="demo" name="eve_hora_hasta" value="<?php echo $evento[7] ?>"  required>                 
+            </div>
+		<br>
+		<div class="form-group">
+		<label class="form-label">descripcion</label>
+		<input class="form-control" type="text" name="eve_descripcion" value="<?php echo $evento[8] ?>"  required>
+	</div>
+		<br>
+			<!-- <input type="hidden" value="" name="Geo_x" id="ltn"> 
+            <input type="hidden" value="" name="Geo_y" id="lng"> -->
+            <div class="input-field col s12">
+          <select id="first_name" type="text" class="validate" required name="eve_estado" value="<?php echo $evento[12] ?>"required>
+            <option value="pendiente">pendiente</option>
+            <option value="publicado">publicado</option>
+            <option value="terminado">terminado</option>
+          </select>
+        </div>
+		<div class="file-field input-field col s12">
+		    <div class="btn">
+		      <span>imagen</span>
+		      <input type="file" name="eve_imagen[]">
+		    </div>
+		    <div class="file-path-wrapper">
+		      <input class="file-path validate"  type="text" placeholder="Puede subir mas de una imagen" name="eve_imagen" value="<?php echo $evento[9] ?>" >
+		    </div>
+		  </div>
 	<div class="form-group">
 		<button name="accion" value="u" class="btn btn-primary">Actualizar</button>
 	</div>
