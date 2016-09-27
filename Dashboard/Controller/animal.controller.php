7794db99e35bebbf2de1830a225caa7bb5f9de35
Andrea0906 <saorozco17@misena.edu.co>
2016-09-27 10:42:55 -0500

    .-.

--- Dashboard/Controller/animal.controller.php
+++ Dashboard/Controller/animal.controller.php
@@ -29,20 +29,26 @@
 			} catch (Exception $e) {
 				$mensaje = "Ha ocurrido un error, el error fue :".$e->getMessage()." en ".$e->getFile()." en la linea ".$e->getLine();
 			}
-			//header("Location: ../View/dashboard.php?p=".base64_encode("mis_mascotas")."&m=$mensaje");
+			header("Location: ../View/dashboard.php?p=".base64_encode("mis_mascotas")."&m=$mensaje");
 
 
 			break;
 			case 'u':
 			$ani_cod_animal						=$_POST["ani_cod_animal"];
-			$ra_cod_raza						=$_POST["ra_cod_raza"];
-      $ani_nombre             = $_POST["ani_nombre"];
-      $ani_esterilizado     = $_POST["ani_esterilizado"];
-      $ani_edad             = $_POST["ani_edad"];
-      $ani_descripcion      = $_POST["ani_descripcion"];
-      $ani_numero_microchip	= $_POST["ani_numero_microchip"];
-
+			$ra_cod_raza			=$_POST["ra_cod_raza"];
+				$org_cod_organizacion	=$_POST["org_cod_organizacion"];
+				$ani_nombre             = $_POST["ani_nombre"];
+		      	$ani_esterilizado     	= $_POST["ani_esterilizado"];
+		     	$ani_edad             	= $_POST["ani_edad"];
+		      	$ani_descripcion      	= $_POST["ani_descripcion"];
+		     	$ani_numero_microchip	= $_POST["ani_numero_microchip"];
+		      	$ani_sexo				= implode(",", $_POST["ani_sexo"]);
+		      	$ani_imagen   			= $_POST["ani_imagen"];
+		      	$count_galeria		= count($_FILES['ani_imagen']['name']);
 			try {
+				if($count_galeria >= 1){ 
+					include("Upload_ani_image.php");
+				} 
 				Gestion_animal::Update($ra_cod_raza,$ani_nombre,$ani_esterilizado,$ani_edad,$ani_descripcion,$ani_numero_microchip,$ani_cod_animal);
 				$mensaje = "Se actializo exitosamente";
 			} catch (Exception $e) {
--- Dashboard/Model/animal.class.php
+++ Dashboard/Model/animal.class.php
@@ -48,15 +48,15 @@
 			floopets_BD::Disconnect();
 		}
 
-		function Update($ra_cod_raza,$ani_nombre,$ani_esterilizado,$ani_edad,$ani_descripcion,$ani_numero_microchip,$ani_cod_animal)
+		function Update($ani_cod_animal,$ra_cod_raza,$ani_nombre,$ani_esterilizado,$ani_edad,$ani_descripcion,$ani_numero_microchip,$ani_sexo,$org_cod_organizacion,$ani_imagen)
 		{
 			//Instanciamos y nos conectamos a la bd
 			$Conexion = floopets_BD::Connect();
 			$Conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			//Crear el query que vamos a realizar
-			$consulta = "UPDATE animal SET ra_cod_raza=?,ani_nombre=?,ani_esterilizado=?,ani_edad=?,ani_descripcion=?,ani_numero_microchip=? WHERE ani_cod_animal = ?" ;
+			$consulta = "UPDATE animal SET ra_cod_raza=?,ani_nombre=?,ani_esterilizado=?,ani_edad=?,ani_descripcion=?,ani_numero_microchip=?,ani_sexo=?, org_cod_organizacion=? ,ani_imagen=? WHERE ani_cod_animal = ?" ;
 			$query = $Conexion->prepare($consulta);
-			$query->execute(array($ra_cod_raza,$ani_nombre,$ani_esterilizado,$ani_edad,$ani_descripcion,$ani_numero_microchip,$ani_cod_animal));
+			$query->execute(array($ra_cod_raza,$ani_nombre,$ani_esterilizado,$ani_edad,$ani_descripcion,$ani_numero_microchip,$ani_sexo,$org_cod_organizacion,$ani_imagen,$ani_cod_animal));
 			floopets_BD::Disconnect();
 		}
 			function Delete($ani_cod_animal)
