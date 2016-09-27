304aa395b7f0a46585025e550587208485d24644
JairoUribe <jairouv@misena.edu.co>
2016-09-27 11:22:54 -0500

    ...

--- Dashboard/View/gestion_voluntarios.php
+++ Dashboard/View/gestion_voluntarios.php
@@ -1,11 +1,13 @@
-<button type="button" name="button">
-  <a href="registrar_voluntarios.php">Nuevo</a>
-</button>
+
 <?php
 
 require_once("../Model/conexion.php");
 require_once("../Model/voluntarios.class.php");
 $voluntarios=gestion_voluntarios::ReadAll();
+
+	@$mensaje = $_REQUEST["m"];
+
+		echo @$mensaje;
  ?>
 
 <div class="container">
@@ -20,45 +22,55 @@ $voluntarios=gestion_voluntarios::ReadAll();
       <td>Foto</td>
       <td>Contraseña</td>
 		</tr>
+
 	</thead>
 
 		<tbody>
-<?php
-			@$mensaje = $_REQUEST["m"];
 
-			echo @$mensaje;
+		<tbody> 
+<div class="row">
+<div class="col l11 offset-l1">
+
+<?php
+			
 
 			foreach ($voluntarios as $row) {
 				echo"
-					<div class='container descrip'>
+					<div class='col l6 descrip'>
                 	<div class='row'>
-                  	<div class='col l4 offset-l2 col m6'>
-                   	<div class='col l6'>
-                        
-                          <a href='../View/actualizar_voluntarios.php?vo=".base64_encode($row["vo_cod_voluntario"])."'>
-                          <i class='small material-icons'>mode_edit</i>
-                          </a>
-                        
-                      </div>
-                      <div class='col l6'>
-                        
-                          <a href='../Controller/voluntarios.controller.php?vo=".base64_encode($row["vo_cod_voluntario"])."&accion=d'>
-                          <i class='small material-icons'>delete</i>
-                          </a>                        
-                        
-                      </div>
-						<div class='col l4'>
+	                  	<div class='col l6 col m6'>
+	                  		<div class='col l7'>
+		                        <span><b>Voluntario:</b></span>
+		                        <li>".$row["vo_nombre"]."</li>
+	                      	</div>
+	                  		<div class='imagenmascota col l12'>
+		                          <img class=' responsive-img' style='width:250px ;height:250px ;' src='img/imagen_voluntario/".$row["vo_nombre"]."/".$row["vo_imagen"]."'>
+		                     	</div>
+	                   		<div class='col l6'>
+	                        	<a class='btn-floating waves-effect' href='../View/dashboard.php?p=".base64_encode('actualizar_voluntarios')."&vo=".base64_encode($row['vo_cod_voluntario'])."'>	
+	                          
+	                          <i class='small material-icons'>mode_edit</i>
+	                          </a>
+	                        
+	                      	</div>
+	                      	<div class='col l6'>
+	                        
+	                          <a class='btn-floating waves-effect waves-light red' href='../Controller/voluntarios.controller.php?vo=".base64_encode($row["vo_cod_voluntario"])."&accion=d'>
+	                          <i class='small material-icons'>delete</i>
+	                          </a>                        
+	                        
+	                      	</div>
+	                     </div>
+						<div class='col l6'>
 		                 	<ul class='descrip'>
 		                   	<label>Codigo</label>
-		                     	  <li>".$row["vo_cod_voluntario"]."</li>
-		                   	<label>Tipo de evento</label>
-		                     	  <li>".$row["vo_nombre"]."</li>
-		                   	<label>Nombre</label>
+		                     	  <li>".$row["vo_cod_voluntario"]."</li>		         
+		                   	<label>Telefono</label>
 		                     	    <li>".$row["vo_telefono"]."</li>
 		                   	<label>Dirección</label>
 		                     	    <li>".$row["vo_direccion"]."</li>
-		                  	 <label>Fecha</label>
-		                     	    <li>".$row["vo_imagen"]."</li>		                                          
+		                	<label>Estado</label>
+		                     	    <li>".$row["vo_estado"]."</li>	                                          
 		                 	</ul>
 		                    
 		             	</div>
@@ -73,8 +85,13 @@ $voluntarios=gestion_voluntarios::ReadAll();
 	}
 
 ?>
+
 		</tbody>
 	
 </table>
 </div>
 </div>
+
+</div>
+</div>
+
