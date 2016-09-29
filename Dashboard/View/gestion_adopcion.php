f80f84bdceb974df2d26ca052ac750387b1bdc01
andreagzmn <avguzman61@misena.edu.co>
2016-09-28 22:53:50 -0500

    detalles

--- Dashboard/View/gestion_adopcion.php
+++ Dashboard/View/gestion_adopcion.php
@@ -29,7 +29,7 @@ $adopcion=Gestion_adopcion::ReadAll();
  </script>
  <!-- esto va en el div de la imagen
   -->
-  <div class="row">
+<div class="row">
 <div class="col l11 offset-l1">
 <?php
 @$mensaje = $_REQUEST["m"];
@@ -37,45 +37,39 @@ echo @$mensaje;
 	foreach ($adopcion as $row) {
     echo"<div class='col l6 descrip'>
             <div class='row'>
-              <div class='col l6 col m6'>
+                <div class='col l6 col m6'>
                   <div class='col l7'>
                   <label>Codigo</label>
                     <span>".$row["ado_cod_adopcion"]."</span>
-                  </div>
-                  <div class='imagenmascota col l12'>
-                      <div style='width:180px ;height:180px ;'>
-                      <img class='circle responsive-img' style='width:180px ;height:180px ;' src='img/imagen_adopcion/".$row["usu_cod_usuario"]."/".$row["ado_imagen"]."'>
-                      </div>
-                  </div>
-                  <div class='col l6'>
-                         <a class='btn-floating waves-effect' href='../View/dashboard.php?p=".base64_encode("actualizar_adopcion")."&ad=".base64_encode($row["ado_cod_adopcion"])."'>
-                         <i class='small material-icons'>edit</i>
+                </div>
+                <div class='imagenmascota col l12'>                      
+                  <img class='responsive-img' style='width:250px ;height:250px ;' src='img/imagen_adopcion/".$row["usu_cod_usuario"]."/".$row["ado_imagen"]."'>                      
+                </div>
+                <div class='col l6'>
+                      <a class='btn-floating waves-effect' href='../View/dashboard.php?p=".base64_encode("actualizar_adopcion")."&ad=".base64_encode($row["ado_cod_adopcion"])."'>
+                        <i class='small material-icons'>edit</i>
                       </a>
-                  </div>
-
-
-                  <div class='col l6'>
+                </div>
+                <div class='col l6'>
                   ";
                   ?>
                   <!-- se reemplaza el href por el onclick que carga el delete -->
-                  <a href='#' id='btntrash' onclick="borrar('<?php echo $row["ado_cod_adopcion"];?>')" class='btn-floating waves-light red waves-effect' >
-                <?php
-                echo "<i class='small material-icons'>delete</i>
+                      <a href='#' id='btntrash' onclick="borrar('<?php echo $row["ado_cod_adopcion"];?>')" class='btn-floating waves-light red waves-effect' >
+                    <?php
+                    echo "<i class='small material-icons'>delete</i>
                   </a>
-                  </div>
+                </div>
               </div>
-              <div class='col l6'>
-                <ul class='descrip'>
-
-                  <span>Fecha :</span>
+                <div class='col l6'>
+                  <ul class='descrip'>
+                    <label>Fecha :</label>
                       <li>".$row["ado_fecha"]."</li>
-                  <span>Codigo animal :</span>
+                    <label>Codigo animal :</label>
                         <li>".$row["ani_cod_animal"]."</li>
-                  <span>Codigo usuario :</span>
+                    <label>Codigo usuario :</label>
                         <li>".$row["usu_cod_usuario"]."</li>
-
-                </ul>
-              </div>
+                  </ul>
+                </div>
             </div>
           </div>
 
