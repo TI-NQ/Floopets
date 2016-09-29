9d85e605aab82fe7e3dbf8bb2a24a709476aca54
Andrea0906 <saorozco17@misena.edu.co>
2016-09-29 07:10:37 -0500

    cuidado :+1:

--- Dashboard/View/gestion_cuidado.php
+++ Dashboard/View/gestion_cuidado.php
@@ -1,10 +1,11 @@
 <!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> -->
 
 <?php
-
 require_once("../Model/conexion.php");
 require_once("../Model/cuidado.class.php");
 $cuidado=Gestion_cuidado::ReadAll();
+
+
  ?>
  <script type="text/javascript">
  function borrar(cod){
@@ -31,34 +32,15 @@ $cuidado=Gestion_cuidado::ReadAll();
 
 <div class="container">
 <div class="row">
-<<<<<<< HEAD
-<table id="datatable" class="display highlight centered responsive-table bordered">
 
-	<thead>
-		<tr>
-			<td>Codigo </td>
-			<td>Nombre</td>
-			<td>Descripcion</td>
-		    <td>Imagen</td>
-		    <td>Acciones</td>
-		</tr>
-	</thead>	
-		<tbody>
-=======
->>>>>>> origin/master
 			<?php
-			@$mensaje = $_REQUEST["m"];
-
-			echo @$mensaje;
 
 			foreach ($cuidado as $row) {
-<<<<<<< HEAD
-				echo"<tr>
-						<td>".$row["cu_cod_cuidado"]."</td>
-						<td>".$row["cu_nombre"]."</td>
-						<td>".$row["cu_descripcion"]."</td>
-            			<td>".$row["galeria"]."</td>
-=======
+         $nombre_cuidado = strtolower(str_replace('ñ', 'n', $row["cu_nombre"]));
+         $nombre_cuidado = strtolower(str_replace(' ', '', $nombre_cuidado));
+
+
+
         echo"<div class='col l6 descrip'>
                 <div class='row'>
                   <div class='col l6 col m6'>
@@ -67,16 +49,14 @@ $cuidado=Gestion_cuidado::ReadAll();
                         <span>".$row["cu_cod_cuidado"]."</span>
                       </div>
                       <div class='imagenmascota col l12'>
-                          <div style='width:180px ;height:180px ;'>
-                          <img class='circle responsive-img' style='width:180px ;height:180px ;' src='img/imagen_cuidado/".$row["cu_nombre"]."/".$row["galeria"]."'>
-                          </div>
+                          <img class='circle responsive-img' style='width:180px ;height:180px ;' src='img/imagen_cuidado/".$nombre_cuidado."/".$row["galeria"]."'>
                       </div>
                       <div class='col l6'>
                              <a class='btn-floating waves-effect' href='../View/dashboard.php?p=".base64_encode("actualizar_cuidado")."&ad=".base64_encode($row["cu_cod_cuidado"])."'>
                              <i class='small material-icons'>edit</i>
                           </a>
                       </div>
->>>>>>> origin/master
+
 
 
                       <div class='col l6'>
@@ -96,8 +76,7 @@ $cuidado=Gestion_cuidado::ReadAll();
                           <li>".$row["cu_nombre"]."</li>
                       <span>Descripcion:</span>
                             <li>".$row["cu_descripcion"]."</li>
-                      <span>Codigo usuario :</span>
-                            <li>".$row["video"]."</li>
+                    
 
                     </ul>
                   </div>
