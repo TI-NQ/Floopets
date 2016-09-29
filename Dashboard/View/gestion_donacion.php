194226c027b9f03c402c95c13b190792fe4b8d58
andreagzmn <avguzman61@misena.edu.co>
2016-09-28 22:24:43 -0500

    Organizando gestion donacion

--- Dashboard/View/gestion_donacion.php
+++ Dashboard/View/gestion_donacion.php
@@ -27,28 +27,33 @@ $donacion=Gestion_donacion::ReadAll();
  }
  </script>
 
-
+<div class="row">
+<div class="col l11 offset-l1">
 <?php
 @$mensaje = $_REQUEST["m"];
 
 echo @$mensaje;
 
 foreach ($donacion as $row) {
-	echo"<div class='container descrip'>
+	echo"<div class='col l6  descrip'>
              <div class='row'>
-               <div class='col l4 offset-l2 col m6'>
-                <div class='col l6'>
-
-                       <a href='../View/actualizar_donacion.php?do=".base64_encode($row["don_cod_donacion"])."'>
-                       <i class='small material-icons'>mode_edit</i>
-                       </a>
-
-                   </div>
-                   <div class='imagenmascota col l12'>
+               <div class='col l6 col m6'>
+                	<div class='col l6'>
+                		 <span><b>Donacion :</b></span>
+                        <li>".$row["don_nombre"]."</li>
+                    </div>
+                    <div class='imagenmascota col l12'>
                        <div style='width:180px ;height:180px ;'>
                        <img class='circle responsive-img' style='width:180px ;height:180px ;' src='img/imagen_donacion/".$row["don_nombre"]."/".$row["don_imagen"]."'>
                        </div>
                    </div>
+                   <div class='col l6'>
+                       <a class='btn-floating waves-effect' href='../View/actualizar_donacion.php?do=".base64_encode($row["don_cod_donacion"])."'>
+                       <i class='small material-icons'>mode_edit</i>
+                       </a>
+
+                   </div>
+                   
                    <div class='col l6'>
                         ";
                         ?>
@@ -56,29 +61,30 @@ foreach ($donacion as $row) {
                         <?php echo"
                           <i class='small material-icons'>delete</i>
                           </a>
-
                    </div>
-			<div class='col l4'>
-		               <ul class='descrip'>
-                   <label>Donacion</label>
-                      <li>".$row["don_nombre"]."</li>
-		                 <label>Codigo</label>
-		                     <li>".$row["don_cod_donacion"]."</li>
-                      <label>Tipo donacion</label>
-                          <li>".$row["td_cod_tipo_donacion"]."</li>
-		                 <label>descripcion</label>
-		                     <li>".$row["don_descripcion"]."</li>
-		                 <label>fecha</label>
-		                     <li>".$row["don_fecha"]."</li>
-		               </ul>
-
-		           </div>
+                </div>
+			             <div class='col l6'>
+    		               <ul class='descrip'>
+                         
+      		                 <label>Codigo</label>
+      		                     <li>".$row["don_cod_donacion"]."</li>
+                            <label>Tipo donacion</label>
+                                <li>".$row["td_cod_tipo_donacion"]."</li>
+      		                 <label>descripcion</label>
+      		                     <li>".$row["don_descripcion"]."</li>
+      		                 <label>fecha</label>
+      		                    <li>".$row["don_fecha"]."</li>
+    		               </ul>
+		                </div>
+		        
 		        </div>
-		     </div>
-		  </div>
+		   </div>
 
 
         ";
 
 	}
  ?>
+ </div>
+</div>
+
