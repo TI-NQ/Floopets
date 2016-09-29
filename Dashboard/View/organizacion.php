8ed89a20abb3392c13837c11550582476fa42542
villadac <ccvillada4@misena.edu.co>
2016-09-29 08:20:43 -0500

    :+1:

--- Dashboard/View/organizacion.php
+++ Dashboard/View/organizacion.php
@@ -3,8 +3,14 @@
   require_once ("../Model/organizacion.class.php");
   require_once ("../Model/adopcion.class.php");
 
+  require_once ("../Model/animal.class.php");
+
   $organizacion=Gestion_organizacion::ReadbyID($_SESSION["org_cod_organizacion"]);
   $adopciones=Gestion_adopcion::Readbyorg_cod($_SESSION["org_cod_organizacion"]);
+  $animal=Gestion_animal::Nombresraza($_SESSION["org_cod_organizacion"]);
+
+  // print_r($animal);
+
 
   // print_r($organizacion);
   // print_r($adopciones);
@@ -13,19 +19,20 @@
 <div class="row">
   <div class="col12">
     <h4 class="center"><?php echo $organizacion[2];?></h4>
-    <img class="circle responsive-img" style="width:180px ;height:180px ;" src="img/imagen_organizacion/'.$organizacion['ado_nombre'].'/'.$organizacion['de_imagen'].">
+
+    <img class="circle responsive-img" style="width:180px ;height:180px ;" src="img/imagen_organizacion/'.$organizacion['org_nombre'].'/'.$organizacion['org_logo'].">
 
 
     <div class="col s6 ayuda">
       <h5 class="center">Mascotas para adopcion</h5>
       <?php
-        foreach ($adopciones as $fila) {
+        foreach ($animal as $fila) {
           ?>
           <div class="row" >
             <div class="col s12 mascotas">
               <div class="col s5">
                 <p>Mascota: <?php echo $fila[2]?></p>
-                <p>Raza: <?php echo $fila[1]?></p>
+                <p>Raza: <?php echo $fila[14]?></p>
                 <p>Esterilizado: <?php echo $fila[5]?></p>
               </div>
               <div class="col s5">
