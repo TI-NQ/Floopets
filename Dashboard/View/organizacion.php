a3202879951d7551b319699c219fe0810e400526
andreagzmn <avguzman61@misena.edu.co>
2016-09-28 21:49:50 -0500

    Detalles

--- Dashboard/View/organizacion.php
+++ Dashboard/View/organizacion.php
@@ -2,14 +2,17 @@
   require_once ("../Model/conexion.php");
   require_once ("../Model/organizacion.class.php");
   require_once ("../Model/adopcion.class.php");
+  
   $organizacion=Gestion_organizacion::ReadbyID($_SESSION["org_cod_organizacion"]);
   $adopciones=Gestion_adopcion::Readbyorg_cod($_SESSION["org_cod_organizacion"]);
+  
   // print_r($organizacion);
   // print_r($adopciones);
 ?>
 <div class="row">
   <div class="col12">
     <h4 class="center"><?php echo $organizacion[2];?></h4>
+
     <div class="col s6 ayuda">
       <h5 class="center">Mascotas para adopcion</h5>
       <?php
@@ -20,11 +23,11 @@
               <div class="col s5">
                 <p>Mascota: <?php echo $fila[2]?></p>
                 <p>Raza: <?php echo $fila[1]?></p>
-                <p>Esterilizado: <?php echo $fila[3]?></p>
+                <p>Esterilizado: <?php echo $fila[5]?></p>
               </div>
               <div class="col s5">
-                <p>Descripcion: <?php echo $fila[5]?></p>
-                <p>Sexo: <?php echo $fila[7]?></p>
+                <p>Descripcion: <?php echo $fila[7]?></p>
+                <p>Sexo: <?php echo $fila[9]?></p>
                 <a href="#" class="waves-effect btn">ver mas</a>
               </div>
 
