23476649e82048ee03402011ff96b0aaef31cb3c
andreagzmn <avguzman61@misena.edu.co>
2016-10-03 22:36:04 -0500

    Orden en gestion voluntario

--- Dashboard/View/gestion_voluntarios.php
+++ Dashboard/View/gestion_voluntarios.php
@@ -25,57 +25,48 @@ if ($valores != 0) {
 		echo"
 
 			<div class='col l6 descrip'>
-							<div class='row'>
-									<div class='col l6 col m6'>
-										<div class='col l7'>
-												<span><b>Voluntario:</b></span>
-												<li>".$row["vo_nombre"]."</li>
-											</div>
-										<div class='imagenmascota col l12'>
-													<img class=' responsive-img' style='width:250px ;height:250px ;' src='img/imagen_voluntario/".$row["vo_cod_voluntario"]."/".$row["vo_imagen"]."'>
-											</div>
-										<div class='col l6'>
-												<a class='btn-floating waves-effect' href='../View/dashboard.php?p=".base64_encode('actualizar_voluntarios')."&vo=".base64_encode($row['vo_cod_voluntario'])."'>
+					<div class='row'>
+						<div class='col l6 m6'>
+								<div class='col l7'>
+									<span><b>Voluntario:</b></span>
+									<li>".$row["vo_nombre"]."</li>
 
-												<i class='small material-icons'>mode_edit</i>
-												</a>
+									
+								</div>
+								<div class='imagenmascota col l12'>
+										<img class=' responsive-img' style='width:250px ;height:250px ;' src='img/imagen_voluntario/".$row["vo_cod_voluntario"]."/".$row["vo_imagen"]."'>
+										<a class='btn waves-effect blue lighten-3 right' href='../Controller/voluntarios.controller.php?vo=".base64_encode($row["vo_cod_voluntario"])."&accion=aceptar'>Aceptar</a>
+								</div>
+								<div class='col l6'>
+									<a class='btn-floating waves-effect' href='../View/dashboard.php?p=".base64_encode('actualizar_voluntarios')."&vo=".base64_encode($row['vo_cod_voluntario'])."'>
 
-											</div>
-											<div class='col l6'>
+									<i class='small material-icons'>mode_edit</i>
+									</a>
 
-												<a class='btn-floating waves-effect waves-light red' href='../Controller/voluntarios.controller.php?vo=".base64_encode($row["vo_cod_voluntario"])."&accion=d'>
-												<i class='small material-icons'>delete</i>
-												</a>
+								</div>
+								<div class='col l6'>
 
-											</div>
-											<div class='col l6'>
-												<a class='btn waves-effect blue lighten-3 right' href='../Controller/voluntarios.controller.php?vo=".base64_encode($row["vo_cod_voluntario"])."&accion=aceptar'>Aceptar</a>
+									<a class='btn-floating waves-effect waves-light red' href='../Controller/voluntarios.controller.php?vo=".base64_encode($row["vo_cod_voluntario"])."&accion=d'>
+									<i class='small material-icons'>delete</i>
+									</a>											
 
-
-											</div>
-
-
-
-
-
-									 </div>
-				<div class='col l6'>
-									<ul class='descrip'>
-										<label>Codigo</label>
-												<li>".$row["vo_cod_voluntario"]."</li>
-										<label>Telefono</label>
-													<li>".$row["vo_telefono"]."</li>
-										<label>Dirección</label>
-													<li>".$row["vo_direccion"]."</li>
-									<label>Estado</label>
-													<li>".$row["vo_estado"]."</li>
-									</ul>
-
-							</div>
-					 </div>
+								</div>  
+						</div>
+						<div class='col l4'>
+							<ul class='descrip'>
+								<label>Codigo</label>
+										<li>".$row["vo_cod_voluntario"]."</li>
+								<label>Telefono</label>
+											<li>".$row["vo_telefono"]."</li>
+								<label>Dirección</label>
+											<li>".$row["vo_direccion"]."</li>
+							<label>Estado</label>
+											<li>".$row["vo_estado"]."</li>
+							</ul>
+						</div>
+						</div>
 				</div>
-		</div>
-
+		
 
 		";
 
